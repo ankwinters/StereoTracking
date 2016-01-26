35e63571abcd9d27b60de5b09499c7d704e18cb9
ckp <ckpwinters@gmail.com>
2016-01-26 16:02:31 +0800

    Calibration

--- calibration.cpp
+++ calibration.cpp
@@ -1,4 +1,30 @@
-//
-// Created by lab on 1/25/16.
-//
+#include <cmath>
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include "calibration.h"
 
+double CameraCalib::ComputeReprojErrors( const vector<vector<Point3f> >& objectPoints,
+                                  const vector<vector<Point2f> >& imagePoints,
+                                  const vector<Mat>& rvecs, const vector<Mat>& tvecs,
+                                  const Mat& cameraMatrix , const Mat& distCoeffs,
+                                  vector<float>& perViewErrors)
+{
+    vector<Point2f> imagePoints2;
+    int i, totalPoints = 0;
+    double totalErr = 0, err;
+    perViewErrors.resize(objectPoints.size());
+
+    for( i = 0; i < (int)objectPoints.size(); ++i )
+    {
+        projectPoints( Mat(objectPoints[i]), rvecs[i], tvecs[i], cameraMatrix,  // project
+                       distCoeffs, imagePoints2);
+        err = norm(Mat(imagePoints[i]), Mat(imagePoints2), CV_L2);              // difference
+
+        int n = (int)objectPoints[i].size();
+        perViewErrors[i] = (float) std::sqrt(err*err/n);                        // save for this view
+        totalErr        += err*err;                                             // sum it up
+        totalPoints     += n;
+    }
+
+    return std::sqrt(totalErr/totalPoints);              // calculate the arithmetical mean
+}
--- calibration.h
+++ calibration.h
@@ -1,8 +1,25 @@
-//
-// Created by lab on 1/25/16.
-//
+
 
 #ifndef POEST_CALIBRATION_H
 #define POEST_CALIBRATION_H
+// Single Camera calibration with chessborad photos
+class CameraCalib
+{
+public:
+    bool ComputeKmat();
+    double ComputeReprojErrors( const vector<vector<Point3f> >& objectPoints,
+                                  const vector<vector<Point2f> >& imagePoints,
+                                  const vector<Mat>& rvecs, const vector<Mat>& tvecs,
+                                  const Mat& cameraMatrix , const Mat& distCoeffs,
+                                  vector<float>& perViewErrors);
 
+    virtual bool SaveAllParas();
+};
+//stereo-camera Calibration
+class StereoCameraCalib:public CameraCalib
+{
+public:
+    bool ComputeLensPose();
+    bool SaveAllParas();
+};
 #endif //POEST_CALIBRATION_H
